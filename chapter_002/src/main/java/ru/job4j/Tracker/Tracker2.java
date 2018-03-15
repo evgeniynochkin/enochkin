d0ff300687dc0747ef0d10fa748f9c74e9e24258
evgeniynochkin <nochjo@yandex.ru>
2018-03-15 23:55:06 +0300

    2. Реализовать класс Tracker.[#396]

---
+++ chapter_002/src/main/java/ru/job4j/Tracker/Tracker2.java
@@ -0,0 +1,136 @@
+package ru.job4j.tracker;
+
+import java.util.Random;
+import java.util.Arrays;
+
+/**
+* Класс-обертка Tracker.
+*
+* @author Evgeniy Nochkin (nochjj@yandex.ru)
+* @since 2.0
+*/
+public class Tracker2 {
+	private Item[] items;
+	private int count;
+	
+	public Item[] getItems() {
+		return Arrays.copyOf(this.items, this.items.length);
+	}
+	
+	/**
+	* Добавление заявки.
+	* @param item.
+	*/
+	public void add(Item item) {
+		if (items != null) {
+			count = this.items.length;
+			Item[] array = new Item[count];
+			for (int i = 0; i < items.length; i++) {
+				array[i] = items[i];
+			}
+			items = new Item[count + 1];
+			for (int i = 0; i < items.length - 1; i++) {
+				this.items[i] = array[i];
+			}
+			item.setId(generateId());
+			this.items[count] = item;
+		} else {
+			items = new Item[1];
+			item.setId(generateId());
+			this.items[0] = item;
+		}
+	}
+	
+	/**
+	* Редактирование заявки.
+	* @param id, item.
+	*/
+	public void replace(String id, Item item) {
+		int i = findIndexById(id);
+		items[i] = item;
+	}
+	
+	/**
+	* Удаление заявки.
+	* @param id.
+	*/
+	public void delete(String id) {
+		if (items != null) {
+			for (int i = 0; i < items.length; i++) {
+				if (getItems()[i].getId().equals(id)) {
+					for (int j = i; j < items.length - 1; j++) {
+						items[j] = items[j + 1];
+					}
+				}
+			}
+			items = Arrays.copyOf(items, items.length - 1);
+		}
+	}
+
+	public Item[] getAll() {
+		return items;
+	}
+
+	
+	/**
+	* Получение списка по имени.
+	* @param name.
+	* @return Array of items by name.
+	*/
+	public Item[] findByName(String name) {
+		Item[] rsl = new Item[items.length];
+		count = 0;
+		for (Item zap : items) {
+			if (zap.getName().equals(name)) {
+				rsl[count] = zap;
+				count++;
+			}
+		}
+		return Arrays.copyOf(rsl, count);
+	}
+	
+	/**
+	* Получение заявки по id.
+	* @param id.
+	* @return item.
+	*/
+	public Item findById(String id) {
+		return items[findIndexById(id)];
+	}
+	
+	/**
+	* Метод генерации уникального ключа.
+	* @return id.
+	*/
+	private String generateId() {
+		String id = null;
+		final Random rnd = new Random();
+		do {
+			id = String.valueOf(rnd.nextInt());
+			if (items[0] != null) {
+				for (int i = 0; i < items.length - 1; i++) {
+					if (items[i].getId().equals(id)) {
+						id = null; 
+					}
+				}
+			}
+		} while (id == null);
+		return id;
+	}
+	
+	/**
+	* Получение индекса записи по id.
+	* @param id.
+	* @return index.
+	*/
+	private int findIndexById(String id) {
+		int index = 0;
+		for (int i = 0; i < this.items.length; i++) {
+			if (this.items[i].getId().equals(id)) {
+				index = i;
+				break;
+			} 
+		}
+		return index;
+	}
+}
---
+++ chapter_002/src/test/java/ru/job4j/Tracker/Tracker2Test.java
@@ -0,0 +1,98 @@
+package ru.job4j.tracker;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class Tracker2Test {
+	
+	@Test
+	public void testAddAndFind() {
+		Tracker2 tracker = new Tracker2();
+		Item item = new Item("test1", "testDescription", 123L);
+		tracker.add(item);
+		assertThat(tracker.getItems()[0], is(item));
+	}
+	
+	@Test
+	public void testFindByName() {
+		Tracker2 tracker = new Tracker2();
+		Item item1 = new Item("test1", "testDesc1", 123L);
+		tracker.add(item1);
+		Item item2 = new Item("test2", "testDesc2", 124L);
+		tracker.add(item2);
+		Item item3 = new Item("test3", "testDesc3", 125L);
+		tracker.add(item3);
+		Item item4 = new Item("test2", "testDesc4", 126L);
+		tracker.add(item4);
+		Item item5 = new Item("test4", "testDesc5", 127L);
+		tracker.add(item5);
+		Item item6 = new Item("test2", "testDesc6", 128L);
+		tracker.add(item6);
+		Item[] itemF = {item2, item4, item6};
+		assertThat(tracker.findByName("test2"), is(itemF));
+	}
+	
+	@Test
+	public void testDelete() {
+		Tracker2 tracker = new Tracker2();
+		Item item1 = new Item("test1", "testDesc1", 123L);
+		tracker.add(item1);
+		Item item2 = new Item("test2", "testDesc2", 124L);
+		tracker.add(item2);
+		Item item3 = new Item("test3", "testDesc3", 125L);
+		tracker.add(item3);
+		Item item4 = new Item("test2", "testDesc4", 126L);
+		tracker.add(item4);
+		Item item5 = new Item("test4", "testDesc5", 127L);
+		tracker.add(item5);
+		Item item6 = new Item("test2", "testDesc6", 128L);
+		tracker.add(item6);
+		Item[] itemD = {item1, item2, item3, item5, item6};
+		String rst = tracker.getItems()[3].getId();
+		tracker.delete(rst);
+		assertThat(tracker.getAll(), is(itemD));
+	}
+	
+	@Test
+	public void testFindById() {
+		Tracker2 tracker = new Tracker2();
+		Item item1 = new Item("test1", "testDesc1", 123L);
+		tracker.add(item1);
+		Item item2 = new Item("test2", "testDesc2", 124L);
+		tracker.add(item2);
+		Item item3 = new Item("test3", "testDesc3", 125L);
+		tracker.add(item3);
+		Item item4 = new Item("test2", "testDesc4", 126L);
+		tracker.add(item4);
+		Item item5 = new Item("test4", "testDesc5", 127L);
+		tracker.add(item5);
+		Item item6 = new Item("test2", "testDesc6", 128L);
+		tracker.add(item6);
+		String rstId = tracker.getItems()[4].getId();
+		assertThat(tracker.findById(rstId), is(item5));
+	}
+	
+	@Test
+	public void testReplace() {
+		Tracker2 tracker = new Tracker2();
+		Item item1 = new Item("test1", "testDesc1", 123L);
+		tracker.add(item1);
+		Item item2 = new Item("test2", "testDesc2", 124L);
+		tracker.add(item2);
+		Item item3 = new Item("test3", "testDesc3", 125L);
+		tracker.add(item3);
+		Item item4 = new Item("test2", "testDesc4", 126L);
+		tracker.add(item4);
+		Item item5 = new Item("test4", "testDesc5", 127L);
+		tracker.add(item5);
+		Item item6 = new Item("test2", "testDesc6", 128L);
+		tracker.add(item6);
+		Item item7 = new Item("test7", "testDesc7", 130L);
+		String rstId = tracker.getItems()[2].getId();
+		tracker.replace(rstId, item7);
+		Item[] itemR = {item1, item2, item7, item4, item5, item6}; 
+		assertThat(tracker.getAll(), is(itemR));
+	}
+}
