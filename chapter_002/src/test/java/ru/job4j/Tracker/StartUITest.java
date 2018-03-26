5208d54714f993ca2776e01e0b4a8660a7444efc
evgeniynochkin <nochjo@yandex.ru>
2018-03-26 21:49:34 +0300

    6. Доработать тесты в StartUITest проверяющие выводы данных в консоль. {#33585]

--- chapter_002/src/test/java/ru/job4j/Tracker/StartUITest.java
+++ chapter_002/src/test/java/ru/job4j/Tracker/StartUITest.java
@@ -1,72 +1,63 @@
 package ru.job4j.tracker;
 
 import org.junit.Test;
-
+import org.junit.Before;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class StartUITest {
 	
-	@Test
-	public void whenUserAddItemThenTrackerHasNewItemWithSameName() {
-		Tracker2 tracker = new Tracker2();
-		Input input = new StubInput(new String[]{"0", "test name", "desc", "6"});
-		new StartUI(input, tracker).init();
-		assertThat(tracker.getAll()[0].getName(), is("test name"));
-	}
-
-	@Test
-	public void whenUserReplaceItemThenTrackerHasEditItemById() {
-		Tracker2 tracker = new Tracker2();
+	private final Tracker2 tracker = new Tracker2();
+	
+	@Before
+	public void loadOutput() {
 		Item item1 = new Item("test name1", "desc1", 123L);
 		tracker.add(item1);
 		Item item2 = new Item("test name2", "desc2", 123L);
 		tracker.add(item2);
 		Item item3 = new Item("test name3", "desc3", 123L);
 		tracker.add(item3);
-		Input input = new StubInput(new String[]{"2", item2.getId(), "new test name2", "new desc2", "6"});
+	}
+	
+	@Test
+	public void whenUserAddItemThenTrackerHasNewItemWithSameName() {
+		Input input = new StubInput(new String[]{"0", "test name4", "desc4", "6"});
+		new StartUI(input, tracker).init();
+		assertThat(tracker.getAll()[3].getName(), is("test name4"));
+	}
+	
+	@Test
+	public void whenUserWhatLookAllItems() {
+		Input input = new StubInput(new String[]{"1", "6"});
+		new StartUI(input, tracker).init();
+		Item[] rst = {tracker.getAll()[0], tracker.getAll()[1], tracker.getAll()[2]};
+		assertThat(tracker.getAll(), is(rst));
+	}
+
+	@Test
+	public void whenUserReplaceItemThenTrackerHasEditItemById() {
+		Input input = new StubInput(new String[]{"2", tracker.getAll()[1].getId(), "new test name2", "new desc2", "6"});
 		new StartUI(input, tracker).init();
 		assertThat(tracker.getAll()[1].getName(), is("new test name2"));
 	}
 	
 	@Test
 	public void whenUserDeleteItem() {
-		Tracker2 tracker = new Tracker2();
-		Item item1 = new Item("test name1", "desc1", 123L);
-		tracker.add(item1);
-		Item item2 = new Item("test name2", "desc2", 123L);
-		tracker.add(item2);
-		Item item3 = new Item("test name3", "desc3", 123L);
-		tracker.add(item3);
-		Input input = new StubInput(new String[]{"3", item2.getId(), "6"});
+		Input input = new StubInput(new String[]{"3", tracker.getAll()[1].getId(), "6"});
 		new StartUI(input, tracker).init();
 		assertThat(tracker.getAll()[1].getName(), is("test name3"));
 	}
 	
 	@Test
 	public void whenUserFindItemById() {
-		Tracker2 tracker = new Tracker2();
-		Item item1 = new Item("test name1", "desc1", 123L);
-		tracker.add(item1);
-		Item item2 = new Item("test name2", "desc2", 123L);
-		tracker.add(item2);
-		Item item3 = new Item("test name3", "desc3", 123L);
-		tracker.add(item3);
-		Input input = new StubInput(new String[]{"4", item1.getId(), "6"});
+		Input input = new StubInput(new String[]{"4", tracker.getAll()[0].getId(), "6"});
 		new StartUI(input, tracker).init();
 		assertThat(tracker.getAll()[0].getName(), is("test name1"));
 	}
 	
 	@Test
 	public void whenUserFindItemByName() {
-		Tracker2 tracker = new Tracker2();
-		Item item1 = new Item("test name1", "desc1", 123L);
-		tracker.add(item1);
-		Item item2 = new Item("test name2", "desc2", 123L);
-		tracker.add(item2);
-		Item item3 = new Item("test name3", "desc3", 123L);
-		tracker.add(item3);
-		Input input = new StubInput(new String[]{"5", item1.getName(), "6"});
+		Input input = new StubInput(new String[]{"5", tracker.getAll()[2].getName(), "6"});
 		new StartUI(input, tracker).init();
 		assertThat(tracker.getAll()[2].getName(), is("test name3"));
 	}
